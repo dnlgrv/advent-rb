243aa6d38b5e9a38efc55fc1fe685f7be26dd070
Daniel Grieve <dnlgrv@hey.com>
2022-11-21 16:43:14 +0000

    Load solution input from local file

--- lib/advent/solution.rb
+++ lib/advent/solution.rb
@@ -11,19 +11,27 @@ module Advent
       @day = day
     end
 
+    def load_input
+      dir = source_location.dirname
+      File.read(dir.join(".day#{@day}.input.txt"))
+    end
+
     private
 
     def infer_year_and_day_from_file_system
-      class_path, _ = Object.const_source_location(self.class.to_s)
-      class_path = Pathname.new(class_path)
+      path, day = source_location.split
 
-      class_path, day = class_path.split
       day = /day([0-9]+)/.match(day.to_s)[1]
-      year = class_path.basename.to_s
+      year = path.basename.to_s
 
       [year, day].map(&:to_i)
     rescue
       raise Error, "Failed to infer year/day of solution."
+    end
+
+    def source_location
+      class_path, _ = Object.const_source_location(self.class.to_s)
+      Pathname.new(class_path)
     end
   end
 end
--- test/advent/solution_test.rb
+++ test/advent/solution_test.rb
@@ -1,13 +1,15 @@
 require "test_helper"
 
 class Advent::SolutionTest < Minitest::Test
+  def setup
+    require DUMMY_ROOT_PATH.join("2015", "day1.rb").to_s
+
+    @solution = Day1.new
+  end
+
   def test_initialize
-    assert require DUMMY_ROOT_PATH.join("2015", "day1.rb").to_s
-
-    solution = Day1.new
-
-    assert_equal 2015, solution.year
-    assert_equal 1, solution.day
+    assert_equal 2015, @solution.year
+    assert_equal 1, @solution.day
   end
 
   def test_initialize_failure
@@ -21,5 +23,9 @@ class Advent::SolutionTest < Minitest::Test
     assert_raises(Advent::Error, "Failed to infer year/day of solution") do
       NoDay.new
     end
+  end
+
+  def test_load_input
+    assert_equal "2015/day1 input", @solution.load_input.strip
   end
 end
