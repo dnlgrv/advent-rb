6cbfc752cfe1895494d8f4858d72e1b47e5a5b15
Daniel Grieve <dnlgrv@hey.com>
2022-11-22 22:18:10 +0000

    Add method for downloading solution input files

--- lib/advent/input.rb
+++ lib/advent/input.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require "cgi/cookie"
+require "net/http"
+require "uri"
+
 module Advent
   class Input
     def initialize(dir, day:)
@@ -13,6 +17,28 @@ module Advent
 
     def exist?
       File.exist? file_path
+    end
+
+    def download(session, http = Net::HTTP)
+      session_cookie = CGI::Cookie.new("session", session)
+      response = http.get(input_url, {"Cookie" => session_cookie.to_s})
+
+      if response
+        File.write(file_path, response)
+        true
+      else
+        false
+      end
+    end
+
+    private
+
+    def input_url
+      URI("https://adventofcode.com/#{year}/day/#{@day}/input")
+    end
+
+    def year
+      @_year ||= @dir.basename
     end
   end
 end
--- test/advent/input_test.rb
+++ test/advent/input_test.rb
@@ -2,17 +2,47 @@
 
 require "test_helper"
 
+MockHTTP = Struct.new("MockHTTP") do
+  def initialize
+    @responses = {}
+  end
+
+  def add_response(url, cookie, body)
+    @responses[url] = {body: body, cookie: cookie}
+  end
+
+  def get(uri, headers)
+    if (response = @responses[uri.to_s]) && response[:cookie] == headers["Cookie"]
+      response[:body]
+    end
+  end
+end
+
 class Advent::InputTest < Minitest::Test
   def setup
-    @input = Advent::Input.new(DUMMY_ROOT_PATH.join("2015"), day: 1)
+    @input = Advent::Input.new(DUMMY_ROOT_PATH.join("2015"), day: 3)
+
+    @http_mock = MockHTTP.new
+    @http_mock.add_response(
+      "https://adventofcode.com/2015/day/3/input",
+      "session=abc123; path=",
+      "day 3 input"
+    )
   end
 
   def test_exist?
-    assert @input.exist?
-    refute Advent::Input.new(DUMMY_ROOT_PATH.join("2015"), day: 2).exist?
+    assert Advent::Input.new(DUMMY_ROOT_PATH.join("2015"), day: 1).exist?
+    refute @input.exist?
   end
 
   def test_file_path
-    assert_equal DUMMY_ROOT_PATH.join("2015", ".day1.input.txt"), @input.file_path
+    assert_equal DUMMY_ROOT_PATH.join("2015", ".day3.input.txt"), @input.file_path
+  end
+
+  def test_download
+    assert @input.download("abc123", @http_mock)
+    assert_equal "day 3 input", File.read(@input.file_path)
+
+    File.delete(@input.file_path)
   end
 end
