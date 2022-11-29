0954bf4b34ee1f43debab4259b9c6d2e190adb22
Daniel Grieve <dnlgrv@hey.com>
2022-11-29 08:33:34 +0000

    Add root based on finding a config file

--- lib/advent.rb
+++ lib/advent.rb
@@ -11,8 +11,25 @@ module Advent
   class Error < StandardError; end
 
   class << self
+    def root
+      if (location = find_config_location)
+        location
+      else
+        raise Error, "Cannot find advent.yml config file in current or parent directories."
+      end
+    end
+
     def session
       @_session = Session.new
+    end
+
+    private
+
+    def find_config_location
+      Pathname.new(Dir.pwd).ascend do |path|
+        return path if File.exist? path.join(Configuration::FILE_NAME)
+        return nil if path.to_s == "/"
+      end
     end
   end
 end
